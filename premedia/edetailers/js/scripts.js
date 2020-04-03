7ed036cd6b3ea8af4ff40fd841db2bdab0b2cdbc
rohan <rohanb10@gmail.com>
2020-04-03 18:12:47 +0530

    google geocharts instead of jqvmap, fix animations on cert page, navbar

--- premedia/edetailers/js/scripts.js
+++ premedia/edetailers/js/scripts.js
@@ -1,16 +1,16 @@
 new fullpage('#fullpage', {
 	licenseKey:'8C4EBEC6-9FCF4B75-B31BA128-7DF9ADB6',
 	autoScrolling:true,
-	// anchors:['home', 'eDetailers', 'certifications','services', 'how-we-work', 'map','about', 'contact'],
-	navigation: true,
-	navigationTooltips: ['Home', 'eDetailers', 'Certifications','Services', 'How We Work', 'Map','About', 'Contact'],
+	anchors: ['home', 'eDetailers', 'certifications','services', 'how-we-work', 'map','about', 'contact'],
+	menu: '#navigation-menu',
+	// navigation: true,
+	// navigationTooltips: ['Home', 'eDetailers', 'Certifications','Services', 'How We Work', 'Map','About', 'Contact'],
 	responsiveWidth: 600,
-	css3: true,
+	paddingTop: '3em',
 	slideSelector: null,
-	scrollbar: true,
 });
 
-fullpage_api.setAllowScrolling(true);
+// fullpage_api.setAllowScrolling(true);
 
 $('.slider').bbslider({
 	auto: true,
@@ -21,7 +21,7 @@ $('.slider').bbslider({
 	pagerWrap: '.pager'
 });
 
-
+/*
 const COUNTRIES = ['ch', 'gb', 'at', 'dk', 'de', 'fr', 'ie', 'mx', 'ar', 'sg', 'kr', 'my', 'be', 'hk', 'au', 'us', 'za'];
 $(document).ready(function() {
 	$('#vmap').vectorMap({
@@ -55,24 +55,73 @@ $(document).ready(function() {
 		}
 	});
 });
+*/
+// const COUNTRIES = ['ch', 'gb', 'at', 'dk', 'de', 'fr', 'ie', 'mx', 'ar', 'sg', 'kr', 'my', 'be', 'hk', 'au', 'us', 'za'];
 
-function openCertModal(el, certName) {
-	const rect = el.getBoundingClientRect();
-	var cords = {x: window.scrollX + (rect.left + rect.right) / 2, y: window.scrollY + (rect.top + rect.bottom) / 2};
+google.charts.load('current', {
+	'packages':['geochart'],
+	'mapsApiKey': 'AIzaSyCAaVmcBfrA0mCjIVl0ljNBhCh4Ciy2mOk'
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+	var options = {
+		backgroundColor: {strokeWidth: '3px'},
+		colorAxis: {colors: ['#3A3A9D']}, // $darkBlue
+		displayMode: 'markers',
+		domain: 'IN',
+		magnifyingGlass: {enable: true, zoomFactor: 3},
+		tooltip: {textStyle: {color: '#000'},showColorCode: true},
+		sizeAxis: {maxSize: 5},
+		legend: 'none'
+	}
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Country');
+	data.addColumn('number', 'Value');
+	data.addColumn({type:'string', role:'tooltip', p: {html: true}});
+
+
+	data.addRows([
+		[{v: 'Switzerland',f:'Switzerland'}, 5, 'Company 231'],
+		[{v: 'United Kingdom',f:'United Kingdom'}, 5, 'Company 2314'],
+		[{v: 'Austria',f:'Austria'}, 5, 'Company 2231'],
+		[{v: 'Denmark',f:'Denmark'}, 5, 'Company 2351'],
+		[{v: 'Germany',f:'Germany'}, 5, 'Company 2u31'],
+		[{v: 'France',f:'France'}, 5, 'Company 2231'],
+		[{v: 'Ireland',f:'Ireland'}, 5, 'Company 23b1'],
+		[{v: 'Mexico',f:'Mexico'}, 5, 'Company 2g31'],
+		[{v: 'Argentina',f:'Argentina'}, 5, 'Company 231333'],
+		[{v: 'Singapore',f:'Singapore'}, 5, 'Company 2ff31'],
+		[{v: 'South Korea',f:'South Korea'}, 5, 'Company 234441'],
+		[{v: 'Malaysia',f:'Malaysia'}, 5, 'Company 20931'],
+		[{v: 'Belgium',f:'Belgium'}, 5, 'Company 232321'],
+		[{v: 'Hong Kong',f:'Hong Kong'}, 5, 'Company 211131'],
+		[{v: 'Australia',f:'Australia'}, 5, 'Company 2551'],
+		[{v: 'United States',f:'United States'}, 5, 'Company 231444'],
+		[{v: 'South Africa',f:'South Africa'}, 5, 'Company hello there'],
+	]);
+
+	var chart = new google.visualization.GeoChart(document.getElementById('gmap'));
+	chart.draw(data, options);
+
+}
+
+function openCertModal(element, certName) {
+	const rect = element.getBoundingClientRect();
+	var cords = {x: window.scrollX + (rect.left + rect.right) / 2 , y: window.scrollY + (rect.top + rect.bottom) / 2};
 	createCircleAnimation(cords);
-
-	$('#'+certName).css({'display':'block'});
-
+	$(element).addClass('active');
+	
 	$('#cert-modal').css({'display':'flex', 'animation-name':'circle-in'});
-	$('.cert-content').css({'animation-name':'fade-in'});
-	$('fp-nav').css({'display':'none'});
+	$('#'+certName).css({'display':'block'});
+	$('.modal-content').delay(600).fadeIn(300);
+	$('body').addClass('dark-bg');
 }
 function closeCertModal() {
-	$('.cert-content').css('animation-name','fade-out');
-	$('#cert-modal').css({'animation-name':'circle-out'});
-	$('fp-nav').css({'display':'block'});
-	$('#veeva, #oce, #econtent').css({'display':'none'});
-	// document.getElementById('cert-modal').style.animationName = "circle-out";
+	$('.modal-content, #veeva, #oce, #econtent').fadeOut(300);
+	$('#cert-modal').delay(150).css({'animation-name':'circle-out'});
+	$('.cert-logo-container').removeClass('active');
+	$('body').removeClass('dark-bg');
 }
 
 function createCircleAnimation(cords) {
