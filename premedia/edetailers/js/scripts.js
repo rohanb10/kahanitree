5e88f98e7b53ddf3531e543f891112b090bd8df6
rohan <rohanb10@gmail.com>
2020-04-16 13:30:04 +0530

    remove IntersectionObserver and replace with throttled onScroll event

--- premedia/edetailers/js/scripts.js
+++ premedia/edetailers/js/scripts.js
@@ -1,16 +1,11 @@
 // remove hash from url
 history.pushState('', '', window.location.pathname);
 
-// remove transition delay when clicking hrefs;
-function checkNavbarBG(navItem) {
-	var navbar = $('.nav-container');
-	var destinationSection = document.getElementById($(navItem).prop('hash').slice(1));
-	navbar.addClass('no-delay');
-	if (destinationSection.classList.contains('dark-bg')) {
-		navbar.addClass('dark-bg');
+// Escape key closes modal
+function escapeToClose(e) {
+	if (e.keyCode == 27) {
+		closeCertModal();
 	}
-	destinationSection.classList.add('animated');
-	setTimeout(() => {navbar.removeClass('no-delay')}, 1000);
 }
 
 // Open certification modal + animation
@@ -27,14 +22,7 @@ function openCertModal(element, certName) {
 	document.addEventListener('keydown', escapeToClose);
 }
 
-// Escape key closes modal
-function escapeToClose(e) {
-	if (e.keyCode == 27) {
-		closeCertModal();
-	}
-}
-
-// close modal animation
+// close certification modal + animation
 function closeCertModal() {
 	$('.modal-content, #veeva, #oce, #mitouch').fadeOut(300);
 	$('#cert-modal').delay(150).css({'animation-name':'circle-out'}).delay(700).toggle(1);
@@ -58,7 +46,6 @@ function createCircleAnimation(cords) {
 		to:   {'clip-path': 'circle(  0% at '+ xy + ')', '-webkit-clip-path': 'circle(  0% at '+ xy + ')', '-ms-clip-path': 'circle(  0% at '+ xy + ')'},
 	}]);
 }
-// Certifications section end
 
 function howWeWorkHandler(el, id) {
 	$('.work-selector span').removeClass('active');
@@ -73,81 +60,64 @@ function howWeWorkHandler(el, id) {
 }
 
 // Map / Clients section
-	const COUNTRIES = [
-		{latLng: [ 46.82,   8.23], name: 'Switzerland', details: 'eDetailing, Website Development, Emailers'},
-		{latLng: [  1.35, 103.82], name: 'Singapore', details: 'eDetailing, Emailers'},
-		{latLng: [ 25.03, 121.57], name: 'Taiwan', details: 'eDetailing, Emailers'},
-		{latLng: [ -33.92, 18.42], name: 'South Africa', details: 'eDetailing'},
-		{latLng: [ 51.51,   0.13], name: 'United Kingdom', details: 'eDetailing, Website Development'},
-		{latLng: [ 47.51,  14.55], name: 'Austria', details: 'Website Development, Emailers'},
-		{latLng: [ 19.43, -99.13], name: 'Mexico', details: 'eDetailing, Emailers'},
-		{latLng: [ 56.26,   9.50], name: 'Denmark', details: 'Website Development'},
-		{latLng: [-22.91, -43.17], name: 'Brazil', details: 'Emailers'},
-		{latLng: [ 52.52,  13.40], name: 'Germany', details: 'Website Development'},
-		{latLng: [ 22.32, 114.17], name: 'Hong Kong', details: 'eDetailing'},
-		{latLng: [ 46.23,   2.21], name: 'France', details: 'eDetailing, Emailers'},
-		{latLng: [ -37.81,144.96], name: 'Australia', details: 'eDetailing, Emailers'},
-		{latLng: [ 53.14,  -7.69], name: 'Ireland', details: 'Emailers'},
-		{latLng: [ 40.42,  -3.70], name: 'Spain', details: 'eDetailing'},
-		{latLng: [ 37.57, 126.98], name: 'South Korea', details: 'eDetailing, Emailers'},
-		{latLng: [ 40.06, -74.41], name: 'United States', details: 'eDetailing, Website Development'}, //NJ
-		{latLng: [ 52.23,  21.01], name: 'Poland', details: 'Emailers'},
-		{latLng: [  4.21, 101.98], name: 'Malaysia', details: 'eDetailing, Website Development, Emailers'},
-		{latLng: [ 50.85,   4.35], name: 'Belgium', details: 'eDetailing, Emailers'},
-	]
+const COUNTRIES = [
+	{latLng: [ 46.82,   8.23], name: 'Switzerland', details: 'eDetailing, Website Development, Emailers'},
+	{latLng: [  1.35, 103.82], name: 'Singapore', details: 'eDetailing, Emailers'},
+	{latLng: [ 25.03, 121.57], name: 'Taiwan', details: 'eDetailing, Emailers'},
+	{latLng: [ -33.92, 18.42], name: 'South Africa', details: 'eDetailing'},
+	{latLng: [ 51.51,   0.13], name: 'United Kingdom', details: 'eDetailing, Website Development'},
+	{latLng: [ 47.51,  14.55], name: 'Austria', details: 'Website Development, Emailers'},
+	{latLng: [ 19.43, -99.13], name: 'Mexico', details: 'eDetailing, Emailers'},
+	{latLng: [ 56.26,   9.50], name: 'Denmark', details: 'Website Development'},
+	{latLng: [-22.91, -43.17], name: 'Brazil', details: 'Emailers'},
+	{latLng: [ 52.52,  13.40], name: 'Germany', details: 'Website Development'},
+	{latLng: [ 22.32, 114.17], name: 'Hong Kong', details: 'eDetailing'},
+	{latLng: [ 46.23,   2.21], name: 'France', details: 'eDetailing, Emailers'},
+	{latLng: [ -37.81,144.96], name: 'Australia', details: 'eDetailing, Emailers'},
+	{latLng: [ 53.14,  -7.69], name: 'Ireland', details: 'Emailers'},
+	{latLng: [ 40.42,  -3.70], name: 'Spain', details: 'eDetailing'},
+	{latLng: [ 37.57, 126.98], name: 'South Korea', details: 'eDetailing, Emailers'},
+	{latLng: [ 40.06, -74.41], name: 'United States', details: 'eDetailing, Website Development'}, //NJ
+	{latLng: [ 52.23,  21.01], name: 'Poland', details: 'Emailers'},
+	{latLng: [  4.21, 101.98], name: 'Malaysia', details: 'eDetailing, Website Development, Emailers'},
+	{latLng: [ 50.85,   4.35], name: 'Belgium', details: 'eDetailing, Emailers'},
+]
 
-	// Render the map (jVectorMap 2.0.5)
-	$(function(){
-		$('#jvectormap').vectorMap({
-			map: 'world_mill',
-			backgroundColor: '#FFF',
-			zoomMax: 4,
-			zoomMin: 0.7,
-			zoomOnScroll: false,
-			zoomStep: 1.6,
-			regionStyle: {
-				initial: {
-					'fill': '#c5c5c5',
-					'fill-opacity': 0.8,
-				},
-				hover: {
-					'fill': '#c5c5c5',
-					'cursor': 'initial',
-				}
+// Render the map (jVectorMap 2.0.5)
+function renderMap() {
+	$('#jvectormap').vectorMap({
+		map: 'world_mill',
+		markers: COUNTRIES,
+		backgroundColor: '#FFF',
+		zoomMax: 4, zoomMin: 0.7, zoomOnScroll: false, zoomStep: 1.6,
+		regionStyle: {
+			initial: {
+				'fill': '#c5c5c5', 'fill-opacity': 0.8,
 			},
-			markerStyle: {
-				'initial': {
-					'fill': '#3A3A9D',
-					'fill-opacity': 0.8,
-					'stroke': '#83D0F4',
-					'r': 6,
-				},
-				hover: {
-					'fill': '#3A3A9D',
-					'stroke': 'black'
-				}
+			hover: {
+				'fill': '#c5c5c5', 'cursor': 'initial',
+			}
+		},
+		markerStyle: {
+			initial: {
+				'fill': '#3A3A9D', 'fill-opacity': 0.8, 'stroke': '#83D0F4', 'r': 6,
 			},
-			focusOn: {
-				x: 0.5,
-				y: 0.5,
-				scale: 0.9,
-			},
-			markers: COUNTRIES,
-			onRegionTipShow: function(e){
-				e.preventDefault();
-			},
-			// Intercept the tooltip shown on hover by adding company name
-			onMarkerTipShow: function(e, tip, code){
-				tip[0].innerHTML = markerTooltipBuilder(code);
-			},
-		});
+			hover: {
+				'fill': '#3A3A9D', 'stroke': 'black'
+			}
+		},
+		focusOn: {
+			x: 0.5, y: 0.5, scale: 0.9,
+		},
+		onRegionTipShow: function(e){e.preventDefault()},
+		// Intercept the tooltip shown on hover by adding company name
+		onMarkerTipShow: function(e, tip, code){
+			tip[0].innerHTML = '<div class="country-name">'+ COUNTRIES[code].name + '</div><div>' + COUNTRIES[code].details + '</div>';
+		},
 	});
-
-//Customise the tooltip for the map
-function markerTooltipBuilder(code) {
-	return '<div class="country-name">'+ COUNTRIES[code].name + '</div><div>' + COUNTRIES[code].details + '</div>';
 }
 
+// Clientside form validation
 function validateContactForm(form){
 	var invalidName = !form.name.value.match(/^[a-z ,.'-]+$/i);
 	document.getElementById('error-name').innerHTML = invalidName ? 'Valid name required' : '&nbsp;';
@@ -183,13 +153,14 @@ function validateContactForm(form){
 
 //close hamburger when link is clicked;
 function mobileNavigate(el) {
-	document.location.hash = '#' + el.dataset.name;
-	setTimeout(() => {
-		document.getElementById("hamburger").checked = false;
+	document.location.hash = el.dataset.name;
+	document.getElementById("hamburger").checked = false;
+	setTimeout(function() {
 		$('nav').addClass('scrolling');
-	}, 250);
+	}, 300);
 }
 
+// After page completes loading
 $(window).load(function() {
 	$('#process').fadeToggle();
 	$('.slider').bbslider({
@@ -202,4 +173,5 @@ $(window).load(function() {
 	});
 	$('.slider').fadeTo(1,1);
 	checkScrollMethod();
+	renderMap();
 });
--- premedia/edetailers/js/scroll.js
+++ premedia/edetailers/js/scroll.js
@@ -1,25 +1,55 @@
-// Use IntersectionObserver for desktop scrolling (add .active class to navbar)
-// observe page position for animations
 if (!isIE){
-	// Callback for IntersectionObserver event
-	const callback = function (entries) {
-		entries.forEach(function (entry) {
-			const {target} = entry;
-			if (entry.intersectionRatio >= 0.01) {
-				if (target.classList.contains('dark-bg')) {
-					$('.nav-container').addClass('dark-bg');
-				} else {
-					$('.nav-container').removeClass('dark-bg');
+
+	// jQuery extension to check if element is on screen
+	$.fn.isInViewport = function() {
+		return ($(this).offset().top + $(this).outerHeight()) > $(window).scrollTop() && $(this).offset().top < ($(window).scrollTop() + $(window).height())
+	};
+
+	var now = Date.now || function() {
+		return new Date().getTime();
+	};
+
+	// UnderscoreJS _.throttle(...) function
+	function throttle(func, wait, options) {
+		var timeout, context, args, result;
+		var previous = 0;
+		if (!options) options = {};
+
+		var later = function() {
+			previous = options.leading === false ? 0 : now();
+			timeout = null;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		};
+
+		var throttled = function() {
+			var _now = now();
+			if (!previous && options.leading === false) previous = _now;
+			var remaining = wait - (_now - previous);
+			context = this;
+			args = arguments;
+			if (remaining <= 0 || remaining > wait) {
+				if (timeout) {
+					clearTimeout(timeout);
+					timeout = null;
 				}
-				target.classList.add('animated');
-				$('.nav-item').removeClass('active');
-				$('.nav-item[data-name=' + target.id +']').addClass('active');
+				previous = _now;
+				result = func.apply(context, args);
+				if (!timeout) context = args = null;
+			} else if (!timeout && options.trailing !== false) {
+				timeout = setTimeout(later, remaining);
 			}
-		});
+			return result;
+		};
+
+		throttled.cancel = function() {
+			clearTimeout(timeout);
+			previous = 0;
+			timeout = context = args = null;
+		};
+
+		return throttled;
 	}
-	const observer = new IntersectionObserver(callback, {
-		threshold: 0.01
-	});
 
 	// Scroll down to hide navbar, scroll up to show
 	var lastScrollTop = 0; var delta = 5;
@@ -49,23 +79,44 @@ if (!isIE){
 		}, 250);
 	}
 
-	// Check whether to load desktop or mobile handlers for navbar
+	// Check whether to load desktop or mobile scroll handlers for navbar
+	var sectionIDs = ['#home', '#eDetailers','#certifications','#services','#how-we-work','#map','#about','#contact'];
 	function checkScrollMethod() {
 		if(window.innerWidth > 768){
 			$('.section').css('height', window.innerHeight);
-			document.querySelectorAll('.section').forEach(function (section, index){
-				observer.observe(section)
-			});
-			//destroy scroll listener
+
 			$('.scroll-snap-container').unbind();
 			clearInterval(scrollInterval);
+
+			$('.scroll-snap-container').on('scroll', throttle(function() {
+				$('.nav-item').removeClass('active');
+				for (var i = 0; i < sectionIDs.length; i++) {
+					var section = $(sectionIDs[i])
+					if(section.isInViewport()) {
+						// Start animations for .section
+						section.addClass('animated');
+						// Make navbar class active
+						$('.nav-item[data-name=' + sectionIDs[i] +']').addClass('active');
+						// Change to dark navbar if applicable
+						if (section.hasClass('dark-bg')) {
+							$('.nav-container').addClass('dark-bg');
+						} else {
+							$('.nav-container').removeClass('dark-bg');
+						}
+						break;
+					}
+				}
+			}, 100));
+
 			return false;
 		} else {
-			$('.scroll-snap-container').scroll(function(){
+			$('.scroll-snap-container').unbind();
+
+			$('.scroll-snap-container').on('scroll', throttle(function() {
 				scrolling = true;
-			});
+			}, 100));
 			scrollInterval = setScrollingInterval();
-			observer.disconnect()
+
 			return true;
 		}
 	}
