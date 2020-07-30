9294dbc4daea735ed40364152163091ce6371bba
rohan <rohanb10@gmail.com>
2020-07-30 13:06:34 +0530

    chairman's statement page. scroll snapping on homepage

--- premedia/infosys/js/script.js
+++ premedia/infosys/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$('.navbar button, #navigation button').click(function() {
 		$('#navigation, body').toggleClass('nav-open')
+	});
+	$('#search-modal').on('shown.bs.modal', function () {
+		$('#search-input').trigger('focus')
 	})
 });
